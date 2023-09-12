f16f0f9f661d3672d746a9054dfa2ab971009394
the_mr17 <saptakmanna100@gmail.com>
2023-09-12 17:00:29 +0530

    Initial commit

---
+++ app/src/main/java/com/mr_17/vidconnect/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mr_17.vidconnect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
