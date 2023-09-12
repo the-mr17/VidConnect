c31accd126a7791bac98fe2ccf4442e0abb05b12
the_mr17 <saptakmanna100@gmail.com>
2023-09-12 21:02:14 +0530

    Implements welcome page

---
+++ app/src/main/java/com/mr_17/vidconnect/ui/auth/fragments/WelcomeFragment.kt
@@ -0,0 +1,28 @@
+package com.mr_17.vidconnect.ui.auth.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.mr_17.vidconnect.R
+import com.mr_17.vidconnect.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentWelcomeBinding.bind(view)
+
+        binding.apply {
+            btnLogin.setOnClickListener {
+                findNavController().navigate(R.id.action_welcomeFragment_to_loginFragment)
+            }
+            btnCreateAccount.setOnClickListener {
+                findNavController().navigate(R.id.action_welcomeFragment_to_signUpFragment);
+            }
+        }
+    }
+}
